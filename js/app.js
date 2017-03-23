1c1842a4e74e2af7af9f3ef5a6f49678a47c8d95
RyderBlack <ryhad.boughanmi@gmail.com>
2017-03-23 18:04:16 +0100

    add sliders entreprises and annonceurs and bg slider part 2

--- js/app.js
+++ js/app.js
@@ -124,9 +124,34 @@ function cycleImages() {
         $active.css('z-index', 1).show().removeClass('active0'); //reset the z-index and unhide the image
         $next.css('z-index', 3).addClass('active0'); //make the next image the top one
     });
-}
+};
 
 $(document).ready(function() {
     // run every 7s
-    setInterval('cycleImages()', 15000);
-})
+    setInterval('cycleImages()', 7000);
+});
+
+
+jQuery(document).ready(function($) {
+
+      $('#myCarousel').carousel({
+              interval: 5000
+      });
+
+      //Handles the carousel thumbnails
+      $('[id^=carousel-selector-]').click(function () {
+      var id_selector = $(this).attr("id");
+      try {
+          var id = /-(\d+)$/.exec(id_selector)[1];
+          console.log(id_selector, id);
+          jQuery('#myCarousel').carousel(parseInt(id));
+      } catch (e) {
+          console.log('Regex failed!', e);
+      }
+  });
+      // When the carousel slides, auto update the text
+      $('#myCarousel').on('slid.bs.carousel', function (e) {
+               var id = $('.item.active').data('slide-number');
+              $('#carousel-text').html($('#slide-content-'+id).html());
+      });
+});
