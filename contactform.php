f514befa77b104cc71c642b7f3f1d5af967d8ca6
RyderBlack <ryhad.boughanmi@gmail.com>
2017-03-28 13:57:19 +0200

    add twizy animation and fix minor bugs

--- contactform.php
+++ contactform.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $prenom=$HTTP_POST_VARS['prenom'];
 $nom=$HTTP_POST_VARS['nom'];
 $mail=$HTTP_POST_VARS['mail'];
@@ -14,17 +13,18 @@ $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
 
 ////ici on détermine l'expediteur et l'adresse de réponse
-$headers .= "From: $nom, $prenom, $telephone <$mail>\r\nReply-to : $nom, $prenom, $telephone <$mail>\nX-Mailer:PHP";
+$headers .= "From: $nom, $prenom <$mail>\r\nReply-to : $nom <$mail>\nX-Mailer:PHP";
 
 
 $destinataire="ryhad.boughanmi@gmail.com"; //remplacez "webmaster@votre-site.com" par votre adresse e-mail
-$body="$message";
+$body="$commentaire";
 if (mail($destinataire,$body,$headers)) {
 echo "Votre mail a été envoyé<br>";
 } else {
 echo "Une erreur s'est produite";
 }
 ?>
-<p align="center">Vous allez bientot etre redirigé vers la page d'acceuil<br>
-Si vous n'etes pas redirigé au bout de 5 secondes cliquez <a href="http://ryderblack.github.io/greencar">ici
+
+<p>Vous allez bientot etre redirigé vers la page d'accueil<br>
+Si vous n'etes pas redirigé au bout de 5 secondes <a href="http://ryderblack.github.io/greencar">cliquez ici
 </a></p>
